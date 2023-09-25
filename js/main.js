b26365675ab650e5f4fef0371cba23a307e6c4b2
Felipe <106964514+szlipin@users.noreply.github.com>
2023-09-25 12:24:22 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-//Array para guardar a tarefa
-const tarefa = [
-    {'tarefa': 'Ler um livro bom'},
-    {'tarefa': 'Correr uma maratona'}
-];
-
-localStorage.setItem('tarefa', 'Ler algo');
-localStorage.getItem();
